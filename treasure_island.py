730f87802f2befd9da9a5100567447526bc052bf
surendraLongre <murari.longre2366@gmail.com>
2023-06-10 19:12:56 +0530

    modified treasure island project

--- treasure_island.py
+++ treasure_island.py
@@ -34,28 +34,21 @@ left_right=input();
 if left_right=="left":
     print('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
     wait_swim=input()
-    print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
-    color=input()
-    if color=="red":
-        print("It\'s a room full of fire. Game Over.")
-    elif color=="yellow":
-        print("You found the treasure! You Win!")
-    elif color=="blue":
-        print("You enter a room of beasts. Game Over.")
+    if wait_swim=="wait":
+        print("you chose wait")
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+        color=input()
+        if color=="red":
+            print("It\'s a room full of fire. Game Over.")
+        elif color=="yellow":
+            print("You found the treasure! You Win!")
+        elif color=="blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("wrong option. You died Game Over.")
+    elif wait_swim=="swim":
+        print("you eaten by crocodiles.Game Over.")
     else:
-        print("wrong option. You died Game Over.")
+        print("wrong option. You eaten by your lover. Game Over")
 elif left_right=="right":
-    print('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
-    wait_swim=input()
-    print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
-    color=input()
-    if color=="red":
-        print("You chose a door that doesn\'t exist. Game Over.")
-    elif color=="yellow":
-        print("You get attacked by an angry trout. Game Over.")
-    elif color=="blue":
-        print("You fell into a hole. Game Over.")
-    else:
-        print("wrong option. You died Game Over.")
-
-
+    print("You eaten by zombies. Game Over.")
